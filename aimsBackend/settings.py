933f89deee4a9c71d11b9ad2335cfdf93f1a3eaf
john-ngugi <johnngugi0407@gmail.com.com>
2026-03-24 12:14:43 +0000

    cors error fix

--- aimsBackend/settings.py
+++ aimsBackend/settings.py
@@ -160,4 +160,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://192.168.1.148/",
+    "http://192.168.1.148:8000",
+    "http://192.168.1.148/api/",
 ]
